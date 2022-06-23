4d95d0e6e1da45153744ee8935c721a32376e251
manishehues <manish@ehues>
2022-06-23 23:36:00 +0530

    fonts upload

--- wp-config.php
+++ wp-config.php
@@ -80,6 +80,7 @@ $table_prefix = 'msg641_';
  * @link https://wordpress.org/support/article/debugging-in-wordpress/
  */
 define( 'WP_DEBUG', false );
+define('ALLOW_UNFILTERED_UPLOADS', true);
 
 /* Add any custom values between this line and the "stop editing" line. */
 
